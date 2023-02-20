d54c5383bec58f025ccb13891416a9219a11e4d7
jianghang <414359193@qq.com>
2023-02-20 22:55:02 +0800

    添加权限

--- src/class/class.controller.ts
+++ src/class/class.controller.ts
@@ -1,9 +1,10 @@
-import { Super } from '@/common/role/auth.decorator';
+import { Auth, Super } from '@/common/role/auth.decorator';
 import { Body, Controller, Post } from '@nestjs/common';
 import { ClassService } from './class.service';
 import { CreateClassDto } from './dto/create-class.dto';
 
 @Controller('class')
+@Auth()
 export class ClassController {
   constructor(private readonly classService: ClassService) {}
   @Post('create')
