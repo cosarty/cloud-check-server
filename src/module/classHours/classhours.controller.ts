939e6b0a1ae0111cf5be1acb1c1ed68ae270a633
jianghang <414359193@qq.com>
2023-03-28 21:58:58 +0800

    开启任务队列

--- src/module/classHours/classhours.controller.ts
+++ src/module/classHours/classhours.controller.ts
@@ -7,6 +7,7 @@ import { Auth } from '@/common/role/auth.decorator';
 import { ModelsEnum, PickModelType } from '@/models';
 import { Body, Controller, Delete, Inject, Param, Post } from '@nestjs/common';
 import { nanoid } from 'nanoid';
+import { ScheduleService } from '../schedule/schedule.service';
 
 @Controller('classHourse')
 export class ClassHoursController {
@@ -15,29 +16,53 @@ export class ClassHoursController {
     private readonly classHourse: PickModelType<ModelsEnum.ClassHours>,
     @Inject(ModelsEnum.TimingTask)
     private readonly timing: PickModelType<ModelsEnum.TimingTask>,
+
+    private readonly schedule: ScheduleService,
   ) {}
 
   // 创建时间
   @Post('create')
   @Auth()
   async create(@Body() pram: any, @User() user) {
-    const data = await this.classHourse.create(pram, {
-      fields: ['classScheduleId', 'timeId', 'weekDay'],
-    });
+    // 课前几分钟发起签到
+    const keepTime = pram.keepTime;
     // 设置定时任务
     const scheduleName = nanoid();
-    await this.timing.create(
+
+    const rule = await this.schedule.getClassHoursTaskCorn({
+      ...pram,
+      keepTime,
+    });
+    await this.schedule.addTimingTask(scheduleName, rule);
+
+    const timing = await this.timing.create(
       {
         ...pram,
         scheduleName: scheduleName,
         taskName: pram.weekDay + '课程自动轮询',
         userId: user.userId,
+        period: rule,
       },
       {
-        fields: ['classScheduleId', 'taskName', 'scheduleName', 'userId'],
+        fields: [
+          'classScheduleId',
+          'taskName',
+          'scheduleName',
+          'userId',
+          'isPeriod',
+          'period',
+        ],
       },
     );
-    return { message: '设置成功', data };
+
+    await this.classHourse.create(
+      { ...pram, timingId: timing.timingId },
+      {
+        fields: ['classScheduleId', 'timeId', 'weekDay', 'timingId'],
+      },
+    );
+
+    return { message: '设置成功' };
   }
 
   @Delete('del/:id')
@@ -45,10 +70,15 @@ export class ClassHoursController {
     const data = await this.classHourse.findOne({
       where: { classHoursId: param.id },
     });
-
     if (data) {
-      await this.timing.destroy({ where: { timingId: data.timingId } });
-      data.destroy();
+      const ti = await this.timing.findOne({
+        where: { timingId: data.timingId },
+      });
+      await data.destroy();
+      if (ti) {
+        await this.schedule.deleteCron(ti.scheduleName);
+        await ti.destroy();
+      }
     }
     // 删除定时任务
     return { message: '删除成功' };
--- src/module/schedule/schedule.service.ts
+++ src/module/schedule/schedule.service.ts
@@ -1,7 +1,8 @@
+import { WeekNum } from '@/constants/weekEnum';
 import { ModelsEnum, PickModelType } from '@/models';
 import { Inject, Injectable, Logger } from '@nestjs/common';
-import { Cron, Timeout } from '@nestjs/schedule';
-
+import { Cron, SchedulerRegistry, Timeout } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 @Injectable()
 export class ScheduleService {
   private readonly logger = new Logger(ScheduleService.name);
@@ -10,6 +11,11 @@ export class ScheduleService {
     private readonly timingTask: PickModelType<ModelsEnum.TimingTask>,
     @Inject(ModelsEnum.SingTask)
     private readonly singTask: PickModelType<ModelsEnum.SingTask>,
+    @Inject(ModelsEnum.ClassSchedule)
+    private readonly classSchdule: PickModelType<ModelsEnum.ClassSchedule>,
+    @Inject(ModelsEnum.Time)
+    private readonly time: PickModelType<ModelsEnum.Time>,
+    private schedulerRegistry: SchedulerRegistry,
   ) {}
 
   @Timeout(1000)
@@ -17,5 +23,50 @@ export class ScheduleService {
     this.logger.debug('项目启动:开始轮询定时任务');
     //  轮询 timing表 然后查询  isPeriod===true && classSchedule.starDate  classSchedule.endDate && classSchedule.isEnd
     // 如果过期了就设置过期
+  }
+
+  // 添加轮询
+  async addTimingTask(name: string, rule: string) {
+    console.log('rule: ', rule);
+    const job = new CronJob(rule, () => {
+      // 轮询到了就插入一条签到信息数据
+      this.logger.warn(`工作中`);
+    });
+
+    this.schedulerRegistry.addCronJob(name, job);
+    job.start();
+  }
+
+  // 生成规则
+  async getClassHoursTaskCorn({
+    classScheduleId,
+    timeId,
+    weekDay,
+    keepTime,
+  }: any) {
+    let cornTab = 's m h * M w';
+    const schedule = (
+      await this.classSchdule.findByPk(classScheduleId)
+    ).toJSON();
+    const time = (await this.time.findByPk(timeId)).toJSON();
+    const endM = new Date(schedule.endDate).getMonth() + 1;
+    const starM = new Date(schedule.starDate).getMonth() + 1;
+    const endD = new Date(schedule.endDate).getDate();
+    const starD = new Date(schedule.starDate).getDate();
+    const mut = time.startTime.split(':')[1];
+    cornTab = cornTab
+      .replace('w', Object.keys(WeekNum).indexOf(weekDay) + 1 + '')
+      .replace('M', starM === endM ? starM + '' : `${starM}-${endM}`)
+      .replace('h', `${time.startTime.split(':')[0]}`)
+      .replace('m', `${Number(mut) - keepTime * 1}`)
+      .replace('s', `0`);
+
+    return cornTab;
+  }
+
+  // 删除任务
+  deleteCron(name: string) {
+    this.schedulerRegistry.deleteCronJob(name);
+    this.logger.warn(`任务 ${name} 终止!`);
   }
 }
